154693eef6af5b19cce0481575277f1c1e6a199b
HENRYPRITCHARD <47064055+HENRYPRITCHARD@users.noreply.github.com>
2019-01-30 16:47:29 -0500

    Most updated version is STANCOM Swift.
    WARNING: StanCom 4.0, and Protocol not verified to work. 
    Download Reddit API before running.

---
+++ Protocol.py
@@ -0,0 +1,116 @@
+#Setup
+import praw, re, csv, random
+
+
+#Validate Reddit Access
+reddit = praw.Reddit(client_id='Dn_ef002ikq0dw',
+                     client_secret='B_8gGLkYtz6aDmZ4tkP5Dj3BFIo',
+                     password='11365abc',
+                     user_agent='pix3lbot_scrape by /u/pix3lbot',
+                     username='pix3lbot')
+subreddit = reddit.subreddit('pix3lspace')
+space = "\n"
+sep = "----------"
+
+
+#Download 
+def download(code):
+    for submission in subreddit.hot(limit=1000):
+        if re.search(code, submission.title, re.IGNORECASE):
+            b = {}
+            counter = 0
+            for top_level_comment in submission.comments:
+                comment = top_level_comment.body
+                counter = counter + 1
+                b[counter] = comment
+            return b
+
+
+#Fetch
+a = {}
+v = ""
+def fetch(code):
+    a = download(code)
+    for key in a:
+        v = a[key]
+        print(v)
+
+
+#Upload
+def create(code,data):
+    for submission in subreddit.hot(limit=1000):
+        if re.search(code, submission.title, re.IGNORECASE):
+            submission.reply(str(data))
+
+
+#Watch Channel
+c = {}
+def probe(code,location):
+    c = download(code)
+    for key in c:
+        print("down: " + c[key + location])
+        with open("ProbeData.txt", 'r') as fp:
+            counter = 1
+            for row in fp:
+                if row != c[counter]:
+                    print("save: " + row)
+                    with open("ProbeData.txt", 'a') as fp:
+                        fp.write(c[counter] + space)
+                elif row == c[counter]:
+                    print("Up to date")
+                counter = counter + 1
+                probe(code,counter)
+            
+
+    
+    
+                
+        
+
+
+#Controller
+def split(status):
+    if status == "Download":
+        name = input("File Location:" + space)
+        fetch(name)
+    elif status == "New Command Line":
+        name = input("Command Channel:" + space)
+    elif status == "Create":
+        name = input("File Location:" + space)
+        data = input("Content:" + space)
+        create(name,data)
+    elif status == "Probe":
+        name = input("Command Channel:" + space)
+        with open("ProbeData.txt", 'w') as fp:
+            fp.write(sep + space)
+        probe(name,0)
+    else:
+        print("Command Not Recognized")
+
+
+    
+    
+    
+
+
+
+    
+#Run Loop
+def run():
+    status = input(space)
+    split(status)
+    run()
+    
+run()
+
+
+
+
+    
+
+
+
+    
+        
+
+                
---
+++ StanComSwift 5.5.py
@@ -0,0 +1,72 @@
+#Setup
+import praw,re
+
+#Validate Reddit Access
+reddit = praw.Reddit(client_id='Dn_ef002ikq0dw',
+                     client_secret='B_8gGLkYtz6aDmZ4tkP5Dj3BFIo',
+                     password='11365abc',
+                     user_agent='pix3lbot_scrape by /u/pix3lbot',
+                     username='pix3lbot')
+
+subreddit = reddit.subreddit('pix3lspace')
+space = "\n"
+sep = "----------"
+
+def collect(post,local,fp):
+    data,i = {},0
+    if local == 1:
+        for row in fp:
+            data[i] = row[0:len(row)-1]
+            i = i + 1
+        return data
+    elif local == 0:
+        for comment in post.comments:
+            data[i] = comment.body
+            i = i + 1
+        return data
+
+def check(dataO,dataL):
+    if len(dataO) != len(dataL):
+        disc = len(dataO) - len(dataL)
+        for i in range(disc): 
+            with open("localcopy.txt", "a") as fp:
+                fp.write(dataO[len(dataO)-disc+i] + space)
+                print(dataO[len(dataO)-disc+i])
+
+def decode(bypass,thread,command,dataO,dataL,post):
+    if command != "pull":
+        post.reply(str(command))
+        connect(bypass,thread,0)
+    elif command == "pull":
+        check(dataO,dataL)
+        connect(bypass,thread,1)
+
+def connect(bypass,thread,skip):
+    with open("localcopy.txt", "r") as fp:
+        if len(bypass) != 0:
+            for post in subreddit.hot(limit=1000):
+                if re.search(bypass, post.title, re.IGNORECASE):
+                    dataO = collect(post,0,fp)
+                    if skip == 0:
+                        decode(bypass,thread,input(":"),dataO,dataO,post)
+                    else:
+                        dataL = collect(post,1,fp)
+                        decode(bypass,thread,"pull",dataO,dataL,post)
+        else:
+            for post in subreddit.hot(limit=1000):
+                if re.search(thread, post.title, re.IGNORECASE):
+                    options = collect(post,0,fp)
+                    for i in options:
+                        print(options[i])
+                    connect(input(":"),thread,0)
+                
+                   
+                    
+                    
+
+
+
+
+
+
+connect("","54325",0)
